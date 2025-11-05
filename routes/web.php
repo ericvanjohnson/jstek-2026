f0bfe293adc7e100e7d3c774f30b52d98b716cad
Eric Van Johnson <eric@phparch.com>
2025-11-04 20:10:46 -0800

    Changing some of the code to JavaScript to make it more legit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\Models\Conference;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     $conference = app('conference');
 
     return view('welcome', compact('conference'));
 })->name('home');
+
+Route::get('/react', function () {
+    $conference = Conference::first();
+
+    return Inertia::render('Welcome', [
+        'conference' => [
+            'name' => $conference->name,
+            'venue_name' => $conference->venue_name,
+            'venue_city' => $conference->venue_city,
+            'venue_state' => $conference->venue_state,
+            'duration_in_days' => $conference->getDurationInDays(),
+        ],
+    ]);
+})->name('react');
